755691a75daa5ad23c8ca30608d649b11de249f5
hung.vx <hungvx.dev@gmail.com>
2023-03-26 11:39:06 +0700

    config fish

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+  echo The time is (set_color yellow; date +%T; set_color normal)
+
+end
---
+++ fish/functions/git_feature_branch_prepend.fish
@@ -0,0 +1,8 @@
+function git_feature_branch_prepend -d 'Detect prepend of feature branches of git repository'
+  command git rev-parse --git-dir &>/dev/null || return
+  if string match -q '*/feat/*' (git show-ref)
+    echo feat
+    return
+  end
+  echo feature
+end
