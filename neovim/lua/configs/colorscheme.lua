f43301a8996993fae8170c4a58794a4bfa1e280f
hung.vx <hungvx.dev@gmail.com>
2023-04-30 09:53:32 +0700

    update neovim tokyonight theme config

--- neovim/lua/configs/colorscheme.lua
+++ neovim/lua/configs/colorscheme.lua
@@ -1,69 +1,91 @@
-local c = require("tokyonight.colors").setup()
-
-if vim.fn.has "nvim-0.8" then
-  -- Ecovim Colors
-  vim.api.nvim_set_hl(0, "EcovimPrimary", { fg = "#488dff" })
-  vim.api.nvim_set_hl(0, "EcovimSecondary", { fg = "#FFA630" })
-
-  vim.api.nvim_set_hl(0, "EcovimPrimaryBold", { bold = true, fg = "#488DFF" })
-  vim.api.nvim_set_hl(0, "EcovimSecondaryBold", { bold = true, fg = "#FFA630" })
-
-  vim.api.nvim_set_hl(0, "EcovimHeader", { bold = true, fg = "#488DFF" })
-  vim.api.nvim_set_hl(0, "EcovimHeaderInfo", { bold = true, fg = "#FFA630" })
-  vim.api.nvim_set_hl(0, "EcovimFooter", { bold = true, fg = "#FFA630" })
-  -- Tokyonight Colorscheme Specific Config
-  if hvim.colorscheme == "tokyonight" then
-    -- Lines
-    -- vim.api.nvim_set_hl(0, "CursorLineNR", { link = "EcovimSecondary" })
-    -- vim.api.nvim_set_hl(0, "LineNr", { link = "Comment" })
-
-    -- Floats/Windows
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None", fg = "None" })
-    -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "None", fg = "None" })
-    -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "None", fg = "#488DFF" })
-    -- vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "None", fg = "#488DFF" })
-    -- vim.api.nvim_set_hl(0, "BufferTabpageFill", { fg = "None" })
-    -- vim.api.nvim_set_hl(0, "VertSplit", { bg = "None", fg = "#16161e" })
-    -- vim.api.nvim_set_hl(0, "BqfPreviewBorder", { link = "FloatBorder" })
-
-    -- Telescope
-    -- vim.api.nvim_set_hl(0, "TelescopeTitle", { link = "EcovimSecondary" })
-    -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "None", fg = "None" })
-    -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "None", fg = "#488DFF" })
-    -- vim.api.nvim_set_hl(0, "TelescopeMatching", { link = "EcovimSecondary" })
-
-    -- Misc
-    -- vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" })
-    -- vim.api.nvim_set_hl(0, "StatusLine", { bg = "None" })
-    -- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "None" })
-    -- vim.api.nvim_set_hl(0, "rainbowcol1", { fg = c.blue, ctermfg = 9 })
-    -- vim.api.nvim_set_hl(0, "Boolean", { fg = "#F7768E" })
-    -- vim.api.nvim_set_hl(0, "BufferOffset", { link = "EcovimSecondary" })
-
-    -- Completion Menu Colors
-    local highlights = {
-      CmpItemAbbr            = { fg = c.dark3, bg = "NONE" },
-      CmpItemKindClass       = { fg = c.orange },
-      CmpItemKindConstructor = { fg = c.purple },
-      CmpItemKindFolder      = { fg = c.blue2 },
-      CmpItemKindFunction    = { fg = c.blue },
-      CmpItemKindInterface   = { fg = c.teal, bg = "NONE" },
-      CmpItemKindKeyword     = { fg = c.magneta2 },
-      CmpItemKindMethod      = { fg = c.red },
-      CmpItemKindReference   = { fg = c.red1 },
-      CmpItemKindSnippet     = { fg = c.dark3 },
-      CmpItemKindVariable    = { fg = c.cyan, bg = "NONE" },
-      CmpItemKindText        = { fg = "LightGrey" },
-      CmpItemMenu            = { fg = "#e77ebf", bg = "NONE" },
-      CmpItemAbbrMatch       = { fg = "#569CD6", bg = "NONE" },
-      CmpItemAbbrMatchFuzzy  = { fg = "#569CD6", bg = "NONE" },
-    }
-    for group, hl in pairs(highlights) do
-      vim.api.nvim_set_hl(0, group, hl)
-    end
-  end
+local present, tokyonight = pcall(require, "tokyonight")
+if not present then
+  return
 end
--- END
---
--- vim.api.nvim_set_hl(0, "FloatShadow", { bg = "NONE" })
--- vim.api.nvim_set_hl(0, "FloatShadowThrough", { bg = "NONE" })
+
+-- ╭──────────────────────────────────────────────────────────╮
+-- │ Setup Colorscheme                                        │
+-- ╰──────────────────────────────────────────────────────────╯
+tokyonight.setup {
+  style                    = "night",
+  terminal_colors          = true,
+  styles                   = {
+    comments  = { italic = true },
+    keywords  = { italic = true },
+    functions = { italic = true },
+    variables = "NONE",
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars  = "transparent",
+    floats    = "transparent",
+  },
+
+  sidebars                 = {},
+  hide_inactive_statusline = true,
+  dim_inactive             = false,
+  lualine_bold             = false,
+  on_highlights            = function(hl, c)
+    local prompt         = "#FFA630"
+    local text           = "#488dff"
+    local none           = "NONE"
+
+    hl.TelescopeTitle    = { fg = prompt }
+    hl.TelescopeNormal   = { bg = none, fg = none }
+    hl.TelescopeBorder   = { bg = none, fg = text }
+    hl.TelescopeMatching = { fg = prompt }
+
+    -- hl.MsgArea                       = { fg = c.fg_dark }
+
+    -- hl.NormalFloat.bg                = none
+    -- hl.NormalSB.bg                   = none
+    -- hl.FloatBorder.bg                = none
+    -- hl.Pmenu.bg                      = none
+    -- hl.BufferOffset                  = { fg = prompt }
+
+    -- hl.NeoTreeNormal.bg              = none
+    -- hl.NeoTreeNormalNC.bg            = none
+
+    hl.StatusLine        = { bg = none }
+    hl.StatusLineNC.bg   = none
+
+    -- hl.TabLine.bg                    = none
+
+    -- hl.TroubleNormal.bg              = none
+
+    -- hl.LspFloatWinNormal.bg          = none
+    -- hl.LspInfoBorder.bg              = none
+
+    -- -- hl.CmpDocumentation.bg           = none
+    -- -- hl.CmpDocumentationBorder.bg     = none
+    -- -- hl.CmpItemAbbr.bg                = none
+    -- -- hl.CmpItemMenu.bg                = none
+    -- -- hl.CmpItemAbbrMatch.bg           = none
+    -- -- hl.CmpItemAbbrMatchFuzzy.bg      = none
+    -- -- hl.CmpItemKindVariable.bg        = none
+
+    -- hl.CmpItemAbbr                   = { fg = c.dark3, bg = none }
+    -- hl.CmpItemKindClass              = { fg = c.orange }
+    -- hl.CmpItemKindConstructor        = { fg = c.purple }
+    -- hl.CmpItemKindFolder             = { fg = c.blue2 }
+    -- hl.CmpItemKindFunction           = { fg = c.blue }
+    -- hl.CmpItemKindInterface          = { fg = c.teal, bg = none }
+    -- hl.CmpItemKindKeyword            = { fg = c.magneta2 }
+    -- hl.CmpItemKindMethod             = { fg = c.red }
+    -- hl.CmpItemKindReference          = { fg = c.red1 }
+    -- hl.CmpItemKindSnippet            = { fg = c.dark3 }
+    -- hl.CmpItemKindVariable           = { fg = c.cyan, bg = none }
+    -- hl.CmpItemKindText               = { fg = "LightGrey" }
+    -- hl.CmpItemMenu                   = { fg = "#C586C0", bg = none }
+    -- hl.CmpItemAbbrMatch              = { fg = "#569CD6", bg = none }
+    -- hl.CmpItemAbbrMatchFuzzy         = { fg = "#569CD6", bg = none }
+
+    -- hl.MiniStatuslineInactive.bg     = none
+    -- hl.MiniTablineHidden.bg          = none
+    -- hl.MiniTablineModifiedHidden.bg  = none
+    -- hl.MiniTablineModifiedVisible.bg = none
+    -- hl.MiniTablineTabpagesection.bg  = none
+    -- hl.MiniTablineVisible.bg         = none
+  end,
+}
+
+-- vim.api.nvim_set_hl(0, 'BufferTabpageFill', { fg = "None" });
+-- vim.api.nvim_set_hl(0, 'BqfPreviewBorder', { link = 'FloatBorder' })
