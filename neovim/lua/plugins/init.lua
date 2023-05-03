6ae3d43920f671534dbb620f5a2807dc62177b3b
hung.vx <hungvx.dev@gmail.com>
2023-05-03 15:27:06 +0700

    update neovim status col

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -337,9 +337,13 @@ return {
           local builtin = require("statuscol.builtin")
           require("statuscol").setup({
             segments = {
-              { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
-              { text = { "%s" },                  click = "v:lua.ScSa" },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" },             click = "v:lua.ScSa" },
+              {
+                text = { builtin.lnumfunc, " " },
+                condition = { true, builtin.not_empty },
+                click = "v:lua.ScLa"
+              },
             },
           })
         end,
