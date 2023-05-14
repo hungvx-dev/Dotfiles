1d7854a5601a1d14c688fe2abaed2ed2c057274e
hung.vx <hungvx.dev@gmail.com>
2023-05-14 12:55:36 +0700

    update neovim keymaps

--- neovim/after/plugin/keymaps.lua
+++ neovim/after/plugin/keymaps.lua
@@ -5,10 +5,10 @@ local expr_opts = { noremap = true, expr = true, silent = true }
 -- Better escape using jk in insert and terminal mode
 -- keymap("i", "jk", "<ESC>", default_opts)
 -- keymap("t", "jk", "<C-\\><C-n>", default_opts)
-keymap("n", "<C-h>", "<C-w>h", default_opts)
-keymap("n", "<C-j>", "<C-w>j", default_opts)
-keymap("n", "<C-k>", "<C-w>k", default_opts)
-keymap("n", "<C-l>", "<C-w>l", default_opts)
+-- keymap("n", "<C-h>", "<C-w>h", default_opts)
+-- keymap("n", "<C-j>", "<C-w>j", default_opts)
+-- keymap("n", "<C-k>", "<C-w>k", default_opts)
+-- keymap("n", "<C-l>", "<C-w>l", default_opts)
 
 -- Center search results
 keymap("n", "n", "nzzzv", default_opts)
