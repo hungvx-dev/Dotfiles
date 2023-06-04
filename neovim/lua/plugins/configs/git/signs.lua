9814fb86c94106a7a0818da4bae735904218bef2
hung.vx <hungvx.dev@gmail.com>
2023-06-04 20:42:43 +0700

    update git config

---
+++ neovim/lua/plugins/configs/git/signs.lua
@@ -0,0 +1,47 @@
+local setup = {
+  signcolumn = true,
+  signs = {
+    add = { text = hvim.icons.UI.Line.BoldMid },
+    change = { text = hvim.icons.UI.Line.BoldMid },
+    delete = { text = hvim.icons.UI.Line.BoldMid },
+    topdelete = { text = hvim.icons.UI.Line.BoldMid },
+    changedelete = { text = hvim.icons.UI.Line.BoldMid },
+    untracked = { text = hvim.icons.UI.Line.BoldMid },
+  },
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 500,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = "<author_mail> | <author_time:%R, %x %h:%M> - <summary>",
+  on_attach = function(buffer)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, desc)
+      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+    end
+
+    -- stylua: ignore start
+    map("n", "]h", gs.next_hunk, "Next Hunk")
+    map("n", "[h", gs.prev_hunk, "Prev Hunk")
+    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+    map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
+    map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
+    map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
+    map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+    map("n", "<leader>hb", function() gs.blame_line({ full = false }) end, "Blame Line")
+    map("n", "<leader>hd", gs.diffthis, "Diff This")
+    map("n", "<leader>hD", function() gs.diffthis("~") end, "Diff This ~")
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+  end,
+}
+
+local present, signs = pcall(require, "gitsigns")
+if not present then
+  return
+end
+
+signs.setup(setup)
