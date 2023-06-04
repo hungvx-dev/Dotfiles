9662e229d9fae2763069c58c10ed2679c63e6ab5
hung.vx <hungvx.dev@gmail.com>
2023-06-04 20:42:53 +0700

    update lualine config

---
+++ neovim/lua/plugins/configs/lualine/init.lua
@@ -0,0 +1,17 @@
+local style = require "plugins.configs.lualine.style"
+local M = {}
+
+M.setup = function()
+  if #vim.api.nvim_list_uis() == 0 then
+    return
+  end
+
+  local status_ok, lualine = pcall(require, "lualine")
+  if not status_ok then
+    return
+  end
+
+  lualine.setup(style)
+end
+
+return M
---
+++ neovim/lua/plugins/configs/lualine/style.lua
@@ -0,0 +1,70 @@
+local components = require "plugins.configs.lualine.components"
+
+local style = {
+  options = {
+    theme = "tokyonight",
+    globalstatus = true,
+    icons_enabled = hvim.use_icons,
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard", "Outline", "dashboard", "fugitive", "gitcommit", "NvimTree" },
+    ignore_focus = {
+      "NvimTree",
+      "packer",
+      "toggleterm",
+      "dapui_scopes",
+      "dapui_stacks",
+      "dapui_breakpoints",
+      "dapui_watches",
+      "dap-repl",
+    },
+  },
+  sections = {
+    lualine_a = {
+      components.mode,
+      components.branch,
+    },
+    lualine_b = {
+      components.diagnostics,
+    },
+    lualine_c = {
+      components.diff,
+      -- components.filename,
+    },
+    lualine_x = {
+      components.lazy,
+      components.lsp,
+      components.spaces,
+      components.filetype,
+    },
+    lualine_y = { components.location },
+    lualine_z = { components.progress },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { components.filename },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {
+    "aerial",
+    "chadtree",
+    "fern",
+    "fugitive",
+    "fzf",
+    "man",
+    "mundo",
+    "neo-tree",
+    "lazy",
+    "nerdtree",
+    "nvim-dap-ui",
+    "nvim-tree",
+    "symbols-outline",
+    "toggleterm",
+  },
+}
+
+return style
