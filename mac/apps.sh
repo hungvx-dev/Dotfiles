1b69606be6b7851d26b62448f978a56c2de03f9d
hung.vx <hungvx.dev@gmail.com>
2023-06-04 20:41:12 +0700

    update mac apps

--- mac/apps.sh
+++ mac/apps.sh
@@ -1,33 +1,20 @@
 !/bin/sh
 
 brew update
+brew upgrade
 
 ## Tools
 brew install autojump
 brew install tmux
 brew install --cask kitty
 brew install fzf
-sudo apt install lazygit
-
-## Install Nvm/node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-## Install nodejs
-brew install node && node -v && npm -v
-nvm list
-npm i -g yarn
-
-## Install git
-brew install git && git --version
-git config --global user.name "hung.vx"
-git config --global user.email "hungvx.dev@gmail.com"
+brew install lazygit
 
 ## Install IDE
-brew install --cask visual-studio-code
 brew install neovim
 code --version && nvim --version
 
-brew install ripgrep
-brew install fd
+brew install ripgrep fb bat
 pip3 --version && python3 --version
 python3 -m pip install --upgrade pip
 pip3 install neovim
@@ -35,14 +22,22 @@ pip3 install pynvim
 yarn global add neovim
 
 ## Package node - neovim
-npm install -g lite-server eslint_d typescript 
+npm install -g lite-server eslint_d typescript
 
-## Lsp
-npm install -g vscode-langservers-extracted vscode-vue-languageservice
-dockerfile-language-server-nodejs graphql-language-service-cli
-typescript-language-server vls yaml-language-server diagnostic-languageserver
-bash-language-server cssmodules-language-server ls-emmet prettier eslint_d
+## Font
+brew tap homebrew/cask-fonts
 
+## Window Manager
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+
+## Music
+brew install cmus
+brew install cava
+brew install --cask background-music
+
+## VPN
+brew install wireguard-tools
 
 # Neovide
 # brew install rustup-init
