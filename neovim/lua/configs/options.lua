c5a6bb01e01458bbd8909a5b88b31dbafc4d8b1e
hung.vx <hungvx.dev@gmail.com>
2023-06-04 20:42:07 +0700

    update fillchars icon

--- neovim/lua/configs/options.lua
+++ neovim/lua/configs/options.lua
@@ -2,8 +2,14 @@ local opt = vim.opt -- local cmd = vim.cmd
 local g = vim.g
 
 g.mapleader = (hvim.leader == "space" and " ") or hvim.leader
-
-opt.fillchars = { eob = " ", fold = " ", foldsep = "│", foldopen = "", foldclose = "" }
+print(123)
+opt.fillchars = {
+  eob = " ",
+  fold = " ",
+  foldsep = "│",
+  foldopen = "",
+  foldclose = "",
+}
 opt.list = true
 opt.listchars = { tab = "» ", extends = "→", nbsp = "·", eol = "↴", trail = "·" }
 opt.formatoptions = "jqlnt" -- tcqj
