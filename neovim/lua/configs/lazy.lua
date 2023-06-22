f59fe595cdeee190eb0997b2a4324c4d52b1f0f0
hung.vx <hungvx.dev@gmail.com>
2023-06-22 09:19:35 +0700

    update lazy config

--- neovim/lua/configs/lazy.lua
+++ neovim/lua/configs/lazy.lua
@@ -1,33 +1,47 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = require("plugins")
+local plugins = require "plugins"
 local opts = {
-    install = {
-        missing = true
+  spec = {
+    { import = "plugins.colorscheme" },
+    { import = "plugins.util" },
+    { import = "plugins.ui" },
+    { import = "plugins.coding" },
+    { import = "plugins.editor" },
+  },
+  install = {
+    missing = true,
+  },
+  ui = {
+    border = "rounded",
+  },
+  git = {
+    timeout = 120,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = { "gzip", "netrwPlugin", "tarPlugin", "tohtml", "tutor", "zipPlugin" },
     },
-    ui = {
-        border = "rounded"
-    },
-    git = {
-        timeout = 120
-    },
-    performance = {
-        rtp = {
-            disabled_plugins = {"gzip", "netrwPlugin", "tarPlugin", "tohtml", "tutor", "zipPlugin"}
-        }
-    },
-    debug = false,
-    defaults = {
-        lazy = false,
-        version = nil
-    }
+  },
+  debug = false,
+  defaults = {
+    lazy = false,
+    version = nil,
+  },
 }
 
-require("lazy").setup(plugins, opts)
-vim.keymap.set('n', '<Leader>l', '<cmd>Lazy<CR>')
+-- require("lazy").setup(plugins, opts)
+
+require("lazy").setup(opts)
+vim.keymap.set("n", "<Leader>l", "<cmd>Lazy<CR>")
