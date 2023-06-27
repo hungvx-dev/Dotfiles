c7a390f6a1e30c4003a5f754d2ac8e3ee1d93bd4
hung.vx <hungvx.dev@gmail.com>
2023-06-27 11:44:08 +0700

    update tmux script

--- bootstrap/macos/tmux.sh
+++ bootstrap/macos/tmux.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-# brew install tmux
+brew install tmux
+which tic
+
+# run at root this folder.
+/usr/bin/tic -xe tmux-256color terminfo.src
+
+echo "Check true color \n"
+tmux info | grep -e RGB -e Tc
