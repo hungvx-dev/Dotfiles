fb5e0d920dc95eca07f31a3497cba0499faf0c7a
hung.vx <hungvx.dev@gmail.com>
2023-06-27 11:44:17 +0700

    update nvim script

--- bootstrap/macos/neovim.sh
+++ bootstrap/macos/neovim.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-# brew install neovim
+python3 -m pip install --upgrade pip
+python3 -m pip install --user --upgrade pynvim
+pnpm install -g neovim
+python3 --version && pip3 --version
+
+brew install ruby
+gem install neovim
+
+brew install neovim
+nvim --version
