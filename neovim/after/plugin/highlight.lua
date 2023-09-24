dbc335f6c864c9c4fd604232d4e7be68e55f7d4e
Võ Xuân Hùng <hungvx.dev@gmail.com>
2023-09-24 11:32:09 +0700

    update neovim colorshema config

--- neovim/after/plugin/highlight.lua
+++ neovim/after/plugin/highlight.lua
@@ -1,2 +1,3 @@
 vim.cmd [[highlight QuickScopePrimary guifg='#5fffff' ctermfg=155]]
 vim.cmd [[highlight QuickScopeSecondary guibg=NONE guifg='#5fffff' ctermfg=81]]
+vim.cmd [[highlight CursorLine gui=underline cterm=underline guibg=NONE]]
--- neovim/lua/cores/colorscheme.lua
+++ neovim/lua/cores/colorscheme.lua
@@ -21,22 +21,28 @@ tokyonight.setup {
   hide_inactive_statusline = true,
   dim_inactive = false,
   lualine_bold = false,
-  -- on_colors = function(colors)
-  -- end,
+  on_colors = function(colors)
+    -- colors.bg = "#1a1b26"
+    colors.bg_dark = colors.bg
+    colors.bg_float = colors.bg
+    colors.bg_sidebar = colors.bg
+    colors.bg_statusline = colors.bg
+    colors.black = colors.bg
+
+    colors.bg = "NONE"
+    -- colors.bg_dark = colors.none
+    -- colors.bg_float = colors.none
+    -- colors.bg_sidebar = colors.none
+    -- colors.bg_statusline = colors.none
+    -- colors.black = colors.none
+
+    colors.bg_popup = "#1a1b26"
+    -- colors.comment = "#3d59a1"
+  end,
   on_highlights = function(hl)
     local prompt = "#FFA630"
-    local text = "#488dff"
-    local none = "NONE"
 
     hl.CursorLineNr = { fg = "#EFFF00" }
     hl.TelescopeTitle = { fg = prompt }
-    hl.TelescopeNormal = { bg = none, fg = none }
-    hl.TelescopeMatching = { fg = prompt }
-
-    hl.StatusLine = { bg = none }
-    hl.StatusLineNC.bg = none
-    hl.Pmenu.bg = none
-    hl.Pmenu.fg = "#27a1b9"
-    hl.PmenuSbar.bg = none
   end,
 }
