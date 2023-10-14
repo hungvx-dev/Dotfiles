20707ce3bedad0f3f027f5f8e14b3238186b38a5
Võ Xuân Hùng <hungvx.dev@gmail.com>
2023-10-14 10:17:37 +0700

    ref: clean code

--- neovim/lua/utils/init.lua
+++ neovim/lua/utils/init.lua
@@ -1,10 +1,15 @@
 local M = {}
 M.root_patterns = { ".git", "lua" }
 
+function M.get_clients(...)
+  local fn = vim.lsp.get_clients or vim.lsp.get_active_clients
+  return fn(...)
+end
+
 function M.on_attach(on_attach)
   vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
-      local buffer = args.buf
+      local buffer = args.buf ---@type number
       local client = vim.lsp.get_client_by_id(args.data.client_id)
       on_attach(client, buffer)
     end,
@@ -84,7 +89,7 @@ function M.opts(name)
   if not plugin then
     return {}
   end
-  local Plugin = require "lazy.core.plugin"
+  local Plugin = require("lazy.core.plugin")
   return Plugin.values(plugin, "opts", false)
 end
 
