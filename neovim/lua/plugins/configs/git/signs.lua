440dc58fa26a267c0f6ecb2e85aa0b80e5c591d2
Võ Xuân Hùng <hungvx.dev@gmail.com>
2023-10-15 17:37:40 +0700

    feat: update virt_text git neovim

--- neovim/lua/plugins/configs/git/signs.lua
+++ neovim/lua/plugins/configs/git/signs.lua
@@ -10,7 +10,7 @@ local setup = {
   },
   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
-    virt_text = false,
+    virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
     delay = 500,
     ignore_whitespace = false,
