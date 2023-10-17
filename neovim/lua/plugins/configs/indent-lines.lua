37abef5272b934153f3780f89288e17680e1fe39
Võ Xuân Hùng <hungvx.dev@gmail.com>
2023-10-17 14:42:55 +0700

    feat: update lualine config

--- neovim/lua/plugins/configs/indent-lines.lua
+++ neovim/lua/plugins/configs/indent-lines.lua
@@ -1,52 +1,11 @@
 local M = {}
-
 local opts = {
-  enabled = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "startify",
-    "dashboard",
-    "lazy",
-    "NvimTree",
-    "Trouble",
-    "neo-tree",
-    "alpha",
-    "mason",
-  },
-  char = hvim.icons.UI.Line.Left,
-  context_char = hvim.icons.UI.Line.Left,
-  show_trailing_blankline_indent = true,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = true,
-  context_patterns = {
-    "class",
-    "return",
-    "function",
-    "method",
-    "^if",
-    "^while",
-    "jsx_element",
-    "^for",
-    "^object",
-    "^table",
-    "block",
-    "arguments",
-    "if_statement",
-    "else_clause",
-    "jsx_element",
-    "jsx_self_closing_element",
-    "try_statement",
-    "catch_clause",
-    "import_statement",
-    "operation_type",
-  },
+  exclude = { filetypes = { "lazy", "neo-tree", "mason", "alpha", "dashboard" } },
+  indent = { char = hvim.icons.UI.Line.Left },
 }
 
 M.setup = function()
-  local status_ok, indent_blankline = pcall(require, "indent_blankline")
+  local status_ok, indent_blankline = pcall(require, "ibl")
   if not status_ok then
     return
   end
