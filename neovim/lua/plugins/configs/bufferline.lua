2bdf0f59771aee90435cb738918cefe5b3de13d3
Võ Xuân Hùng <hungvx.dev@gmail.com>
2023-10-17 14:33:31 +0700

    feat: clean bufferline config

--- neovim/lua/plugins/configs/bufferline.lua
+++ neovim/lua/plugins/configs/bufferline.lua
@@ -19,66 +19,7 @@ local function diagnostics_indicator(num, _, diagnostics, _)
   return #result > 0 and result or ""
 end
 
--- local transparent = { bg = "none" }
-
--- local hl = {
---   fill = transparent,
---   background = transparent,
---   tab = transparent,
---   tab_selected = transparent,
---   tab_close = transparent,
---   close_button = transparent,
---   close_button_visible = transparent,
---   buffer_visible = transparent,
---   buffer_selected = transparent,
---   numbers = transparent,
---   numbers_visible = transparent,
---   numbers_selected = transparent,
---   diagnostic = transparent,
---   diagnostic_visible = transparent,
---   diagnostic_selected = transparent,
---   hint = transparent,
---   hint_visible = transparent,
---   hint_selected = transparent,
---   hint_diagnostic = transparent,
---   hint_diagnostic_visible = transparent,
---   hint_diagnostic_selected = transparent,
---   info = transparent,
---   info_visible = transparent,
---   info_selected = transparent,
---   info_diagnostic = transparent,
---   info_diagnostic_visible = transparent,
---   info_diagnostic_selected = transparent,
---   warning = transparent,
---   warning_visible = transparent,
---   warning_selected = transparent,
---   warning_diagnostic = transparent,
---   warning_diagnostic_visible = transparent,
---   warning_diagnostic_selected = transparent,
---   error = transparent,
---   error_visible = transparent,
---   error_selected = transparent,
---   error_diagnostic = transparent,
---   error_diagnostic_visible = transparent,
---   error_diagnostic_selected = transparent,
---   modified = transparent,
---   modified_visible = transparent,
---   modified_selected = transparent,
---   duplicate_selected = transparent,
---   duplicate_visible = transparent,
---   duplicate = transparent,
---   separator_selected = transparent,
---   separator_visible = transparent,
---   separator = transparent,
---   indicator_selected = transparent,
---   pick_selected = transparent,
---   pick_visible = transparent,
---   pick = transparent,
---   offset_separator = transparent,
--- }
-
 local setup = {
-  -- highlights = hl,
   options = {
     mode = "buffers",
     numbers = "none",
@@ -147,7 +88,4 @@ M.setup = function()
 
   bufferline.setup { options = setup.options }
 end
-
---stylua: ignore
-
 return M
