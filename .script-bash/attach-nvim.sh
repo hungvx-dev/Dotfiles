7478dfe64a8eb3454aae1a2a8e19b4d41928ae37
Võ Xuân Hùng <hungvx.dev@gmail.com>
2023-11-01 20:14:38 +0700

    feat: add tmux script and alias

--- .script-bash/attach-nvim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-tmux rename-window Dotfiles
-cd $(autojump Dotfiles)
-nvim
--- fish/alias/my_alias.fish
+++ fish/alias/my_alias.fish
@@ -1,6 +1,6 @@
-abbr cl 'clear'
-abbr ex 'exit'
-abbr nv 'nvim'
+abbr cl clear
+abbr ex exit
+abbr nv nvim
 
 # ls
 abbr l 'ls -Flh'
@@ -8,21 +8,22 @@ abbr ll 'ls -Flha'
 abbr ls 'ls -lag'
 
 # Tmux
-abbr t 'tmux'
+abbr t tmux
 abbr ta 'tmux attach'
 abbr tas 'tmux attach-section'
 abbr tk 'tmux kill-server'
 
 # node
-abbr pn 'pnpm'
+abbr pn pnpm
 abbr npm-list 'npm list -g --depth=0'
 
-abbr lz 'lazygit'
+abbr lz lazygit
 
 abbr cmus 'TERM=xterm-kitty cmus'
 abbr cava 'TERM=xterm-kitty cava'
 
 # command script shell
-abbr ide '~/.script-bash/ide.sh'
-abbr dfs '~/.script-bash/attach-nvim.sh Dotfile'
-abbr music '~/.script-bash/music.sh'
+abbr ide '~/.script-bash/tmux/ide.sh'
+abbr ts '~/.script-bash/tmux/sessionizer.sh'
+abbr tw '~/.script-bash/tmux/windowizer.sh'
+abbr tls '~/.script-bash/tmux/lazy-search-session.sh'
