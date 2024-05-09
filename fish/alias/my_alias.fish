6bc77b55f886cefab7a7f14def19beb1f683f2a3
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-05-09 15:25:24 +0700

    fish update alias

--- fish/alias/my_alias.fish
+++ fish/alias/my_alias.fish
@@ -3,9 +3,13 @@ abbr ex exit
 abbr nv nvim
 
 # ls
-abbr l 'ls -Flh'
-abbr ll 'ls -Flha'
-abbr ls 'ls -lag'
+abbr l 'ls -Flha'
+abbr ls 'eza --color=always --icons=always -h -l -g -a --git --time-style=+%Y-%m-%d\ %H:%M'
+abbr ll 'eza --color=always --icons=always -h -l -a --no-filesize --no-user --no-permissions --no-time --git-ignore'
+abbr tree 'eza -T -L=2 --git-ignore'
+# abbr ll 'ls -Flha'
+# abbr ls 'ls -lag'
+
 
 # Tmux
 abbr t tmux
@@ -17,10 +21,10 @@ abbr tk 'tmux kill-server'
 abbr pn pnpm
 abbr npm-list 'npm list -g --depth=0'
 
-abbr lz lazygit
 
 abbr cmus 'TERM=xterm-kitty cmus'
 abbr cava 'TERM=xterm-kitty cava'
+abbr lz 'TERM=xterm-kitty lazygit'
 
 # command script shell
 abbr ide '~/.script-bash/tmux/ide.sh'
