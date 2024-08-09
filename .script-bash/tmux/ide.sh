ba974e714fc83e43d6d6c99a03392e74ba6d8eac
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-08-09 09:41:35 +0700

    update new version karabiner

--- .script-bash/tmux/ide.sh
+++ .script-bash/tmux/ide.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-# Dotfiles Session
+cd
+
+# ------------ Dotfiles Session
 dotfilesSession="Dotfiles"
 dotfilesWindow="dotfiles-nvim"
 # Dotfiles nvim window
 tmux new-session -d -c ~/Dev/Dotfiles -s $dotfilesSession -n $dotfilesWindow
 tmux send-keys "nvim" C-m
 
-# Lazygit window
+# ---- Lazygit window
 lazygitDotfilesWindow="lazygit"
 tmux new-window -c ~/Dev/Dotfiles -t $dotfilesSession:2 -n $lazygitDotfilesWindow
 tmux send-keys "TERM=xterm-kitty lazygit" C-m
 
 tmux split-window -c ~/Dev/Dotfiles -v
 tmux send-keys "cl" C-m
-
 
 # # AMTB session
 # AMTBSession="AMTB-TTHH"
@@ -23,17 +24,17 @@ tmux send-keys "cl" C-m
 # tmux send-keys "cd ./DieuPhap && cl" C-m
 
 # Learning session
-learningSession="Learning"
-learningDir="~/Dev/Learning"
-
-tmux new-session -d -c ~/Dev/Learning -s $learningSession -n Vue
-tmux send-keys "cd ./vue && cl" C-m
-
-tmux new-window -c ~/Dev/Learning -t $learningSession:2 -n typescript
-tmux send-keys "cd ./typescript && cl" C-m
-
-tmux new-window -c ~/Dev/Learning -t $learningSession:3 -n backend
-tmux send-keys "cd ./backend && cl" C-m
+# learningSession="Learning"
+# learningDir="~/Dev/Learning"
+#
+# tmux new-session -d -c ~/Dev/Learning -s $learningSession -n Vue
+# tmux send-keys "cd ./vue && cl" C-m
+#
+# tmux new-window -c ~/Dev/Learning -t $learningSession:2 -n typescript
+# tmux send-keys "cd ./typescript && cl" C-m
+#
+# tmux new-window -c ~/Dev/Learning -t $learningSession:3 -n backend
+# tmux send-keys "cd ./backend && cl" C-m
 
 # Work Session
 # hodfordsSession="Hodfords"
