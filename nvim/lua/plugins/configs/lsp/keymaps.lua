8000fa45367f08bb42bc8b4eb034991a8b6337bd
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-09-22 10:49:17 +0700

    chore(neovim): clean codes

--- nvim/lua/plugins/configs/lsp/keymaps.lua
+++ nvim/lua/plugins/configs/lsp/keymaps.lua
@@ -4,10 +4,10 @@ keymap.set("n", "[d", vim.diagnostic.goto_prev)
 keymap.set("n", "]d", vim.diagnostic.goto_next)
 keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
+keymap.set("n", "<space>rs", ":LspRestart<CR>", { noremap = true, silent = true })
+
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { noremap = true, silent = true, buffer = ev.buf }
 
     keymap.set("n", "gf", function()
@@ -19,21 +19,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
       })
     end, opts)
 
-    keymap.set("n", "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, opts)
+    keymap.set("n", "gd", function()
+      require("telescope.builtin").lsp_definitions({ reuse_win = true })
+    end, opts)
     -- keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
     keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+    keymap.set("n", "gR", vim.lsp.buf.rename, opts)
     keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
     keymap.set("n", "<space>D", "<cmd>Telescope lsp_type_definitions<CR>", opts)
     keymap.set({ "n", "v" }, "ga", vim.lsp.buf.code_action, opts)
     keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-    keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-    keymap.set("n", "<space>rs", ":LspRestart<CR>", opts)
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, opts)
+    keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
   end,
 })
