9f2a14a2924288ed1974f4dce163d573bd56d3c3
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-09-29 11:08:07 +0700

    feat(neovim): change incsearch fg

--- nvim/lua/plugins/configs/colorscheme/tokyonight.lua
+++ nvim/lua/plugins/configs/colorscheme/tokyonight.lua
@@ -1,7 +1,4 @@
-local present, tokyonight = pcall(require, "tokyonight")
-if not present then
-  return
-end
+local tokyonight = require("tokyonight")
 
 -- ╭──────────────────────────────────────────────────────────╮
 -- │ Setup Color scheme                                       │
@@ -23,7 +20,7 @@ tokyonight.setup({
   on_colors = function(colors)
     -- Support transparent
     colors.none = "NONE"
-    colors.bg = "NONE"
+    colors.bg = colors.none
     colors.bg_dark = colors.none
     colors.bg_float = colors.none
     colors.bg_sidebar = colors.none
@@ -33,55 +30,26 @@ tokyonight.setup({
     colors.comment = "#b9b9d9"
     colors.fg_gutter = "#394b70"
 
-    -- No change
-    -- colors.bg_highlight = "#292e42"
-    -- colors.bg_visual = "#283457"
-    colors.rainbow = { "#74a2ff", "#eeab00", "#8fd300", "#00bd9c", "#bd98ff", "#a86dff" }
-
     colors.bg_search = "#0057c4"
     colors.blue = "#74a2ff"
     colors.blue0 = "#0057c4"
     colors.blue1 = "#00c4e0"
-    colors.blue2 = "#00b9d7"
-    colors.blue5 = "#89ddff"
     colors.blue6 = "#8cfffe"
     colors.blue7 = "#004b92"
-    colors.border = "#131525"
-    colors.border_highlight = "#00a2bb"
-    colors.cyan = "#7dcfff"
     colors.dark3 = "#455a9d"
     colors.dark5 = "#6677c6"
-    colors.diff = {
-      add = "#20303b",
-      change = "#1f2231",
-      delete = "#37222c",
-      text = "#394b70",
-    }
     colors.error = "#fb0000"
     colors.fg = "#bdcaff"
     colors.fg_dark = "#9daff8"
     colors.fg_float = "#bdcaff"
     colors.fg_sidebar = "#9daff8"
-    colors.git = {
-      add = "#449dab",
-      change = "#6183bb",
-      delete = "#914c54",
-      ignore = "#545c7e",
-    }
     colors.green = "#8fd300"
     colors.green1 = "#00e1cb"
     colors.green2 = "#00a8ba"
-    colors.hint = "#00bd9c"
-    colors.info = "#00b9d7"
-    colors.magenta = "#bd98ff"
-    colors.magenta2 = "#ff007c"
-    colors.orange = "#ff9e64"
     colors.purple = "#a86dff"
     colors.red = "#ff708c"
     colors.red1 = "#fb0000"
-    colors.teal = "#00bd9c"
     colors.terminal_black = "#334685"
-    colors.todo = "#74a2ff"
     colors.warning = "#eeab00"
     colors.yellow = "#eeab00"
   end,
@@ -91,6 +59,7 @@ tokyonight.setup({
     hl.CursorLine = { bg = colors.none }
     hl.Cursor = { bg = colors.blue1 }
     hl.PmenuSbar = { bg = colors.none }
-    hl.GitSignsCurrentLineBlame = { fg = "#5e647a" }
+    hl.GitSignsCurrentLineBlame = { fg = "#5e6777" }
+    hl.IncSearch.fg = "#1a1b26"
   end,
 })
