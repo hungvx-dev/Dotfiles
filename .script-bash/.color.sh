8f832ebc361983db7556570e292e5824386bfc93
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-10-25 12:44:13 +0700

    feat: add color script

---
+++ .script-bash/.color.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# Escape Sequences
+# literal   \e, \E
+# octal     \033
+# hex       \x1b
+
+# Graphics Codes
+# ESC[0m                reset all modes (styles and colors)
+# ESC[1m    ESC[22m     set bold mode.
+# ESC[2m    ESC[22m     set dim/faint mode.
+# ESC[3m    ESC[23m     set italic mode.
+# ESC[4m    ESC[24m     set underline mode.
+# ESC[5m    ESC[25m     set blinking mode
+# ESC[7m    ESC[27m     set inverse/reverse mode
+# ESC[8m    ESC[28m     set hidden/invisible mode
+# ESC[9m    ESC[29m     set strikethrough mode.
+
+# Color Codes
+# Color             Foreground      Background
+# Black                 30              40
+# Red                   31              41
+# Green                 32              42
+# Yellow                33              43
+# Blue                  34              44
+# Magenta               35              45
+# Cyan                  36              46
+# White                 37              47
+# Default               39              49
+# Bright Black          90              100
+# Bright Red            91              101
+# Bright Green          92              102
+# Bright Yellow         93              103
+# Bright Blue           94              104
+# Bright Magenta        95              105
+# Bright Cyan           96              106
+# Bright White          97              107
+# Reset                 0               0
+
+# 256 Colors
+# ESC[38;5;${color}m   set foreground color
+# ESC[48;5;${color}m   set background color
+
+COLORS="Black Red Green Yellow Blue Magenta Cyan White"
+
+ESC="\033["
+RESET="\033[0m"
+TEXT=" abc "
+
+printf "%-11s" " "
+for color in $COLORS; do
+  i=$((i + 1))
+  printf "${ESC}$((i + 29))m%-18s${RESET}" "$color"
+done
+printf "\n\n"
+
+b=40
+for color in $COLORS; do
+  printf "${ESC}$((b - 10))m%-11s${RESET}" "$color    "
+  for f in $(seq 30 37); do
+    printf "${ESC}${b};${f}m${TEXT}${RESET}    "
+    printf "${ESC}${b};$((f + 60));1m${TEXT}${RESET}    "
+  done
+  printf "\n\n"
+  printf "%-11s" "$color    "
+  for f in $(seq 90 97); do
+    printf "${ESC}${b};${f}m${TEXT}${RESET}    "
+    printf "${ESC}${b};$((f + 60));1m${TEXT}${RESET}    "
+  done
+  printf "\n\n"
+  b=$((b + 1))
+done
+one
