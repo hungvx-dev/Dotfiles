45a41db14160e926a19a3b7e6206aae87d1d7f4e
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-12-01 18:23:35 +0700

    feat(neovim): update todocomment config

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -18,9 +18,9 @@ return {
     "folke/todo-comments.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-    keys ={
-      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment", },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment", },
       { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
       { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
       { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Todo" },
@@ -30,20 +30,29 @@ return {
       signs = true,
       sign_priority = 8,
       keywords = {
-        FIX = {
-          alt = { "FIXME", "BUG", "ISSUE" },
-        },
-        WARN = { alt = { "WARNING" } },
-        PERF = { alt = { "PERFORMANCE", "OPTIMIZE" } },
+        -- FIX: Fix
+        -- TODO: Todo
+        -- HACK: Hack
+        -- WARN: Warn
+        -- PERF: Perf
+        -- NOTE: Note
+        -- TEST: Test
+        FIX = { icon = "" },
+        TODO = { icon = "󰄬" },
+        HACK = { icon = "" },
+        WARN = { icon = "󰀪" },
+        PERF = { icon = "󰅐" },
+        NOTE = { icon = "󱞀" },
+        TEST = { icon = "󱏛" },
+      },
+      colors = {
+        test = { "#FF00FF" },
       },
       highlight = {
         before = "",
         keyword = "wide",
-        after = "",
-        pattern = [[.*<(KEYWORDS)\s*:]],
+        after = "fg",
         comments_only = true,
-        max_line_len = 400,
-        exclude = {},
       },
     },
   },
