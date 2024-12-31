b05941390c2a8a26c72f9a13309306cc1fa3dafa
Vo Xuan Hung <hungvx.dev@gmail.com>
2024-12-31 20:04:33 +0700

    feat(neovim): update statuscol config

--- nvim/lua/plugins/dev/statuscol/lua/statuscol.lua
+++ nvim/lua/plugins/dev/statuscol/lua/statuscol.lua
@@ -91,19 +91,16 @@ function M.setup(opts)
   local id = vim.api.nvim_create_augroup("StatusCol", {})
   if opts.ft_ignore then
     vim.wo.statuscolumn = "%!v:lua.require('statuscol').statuscol()"
-
-    if opts.ft_ignore then
-      vim.api.nvim_create_autocmd("FileType", { group = id, pattern = opts.ft_ignore, command = "setlocal stc=" })
-      vim.api.nvim_create_autocmd("BufWinEnter", {
-        group = id,
-        callback = function()
-          if vim.tbl_contains(opts.ft_ignore, vim.api.nvim_get_option_value("ft", { scope = "local" })) then
-            vim.api.nvim_set_option_value("stc", "", { scope = "local" })
-            vim.api.nvim_set_option_value("foldcolumn", "0", { scope = "local" })
-          end
-        end,
-      })
-    end
+    vim.api.nvim_create_autocmd("FileType", { group = id, pattern = opts.ft_ignore, command = "setlocal stc=" })
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      group = id,
+      callback = function()
+        if vim.tbl_contains(opts.ft_ignore, vim.api.nvim_get_option_value("ft", { scope = "local" })) then
+          vim.api.nvim_set_option_value("stc", "", { scope = "local" })
+          vim.api.nvim_set_option_value("foldcolumn", "0", { scope = "local" })
+        end
+      end,
+    })
   end
   if opts.wrap then
     vim.api.nvim_create_autocmd(
