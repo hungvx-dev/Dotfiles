68510b68480d2b90dd0c5ccc254ce700e34cdc51
Vo Xuan Hung <hungvx.dev@gmail.com>
2025-01-11 22:01:50 +0700

    feat(fish): update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,21 @@
-set -x _ZO_ECHO 1
-set -x _ZO_RESOLVE_SYMLINKS 1
-zoxide init fish | source
-
-starship init fish | source
-
-
-set -x LG_CONFIG_FILE "$HOME/.config/lazygit/config.yml"
-set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
-set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/.ripgreprc"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 set fish_path $HOME/.config/fish
 . $fish_path/kitty.sh
 . $fish_path/alias/init.fish
+
+set -gx _ZO_DATA_DIR "$HOME/Dev/Dotfiles/zoxide"
+set -gx _ZO_ECHO 1
+set -gx _ZO_RESOLVE_SYMLINKS 1
+zoxide init fish --cmd j | source
+
+starship init fish | source
+
+set -x LG_CONFIG_FILE "$HOME/.config/lazygit/config.yml"
+set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/.ripgreprc"
 
 set -g FZF_DEFAULT_COMMAND "fd --type=f --hidden --color=never --exclude .git"
 set -g FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS \
