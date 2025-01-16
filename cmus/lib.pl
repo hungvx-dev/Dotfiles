dd69c8e0afc1175519041dd4ad5100693ec9a061
Vo Xuan Hung <hungvx.dev@gmail.com>
2025-01-16 20:23:46 +0700

    feat(cmus): change colorcheme

---
+++ cmus/lib.pl
@@ -0,0 +1,18 @@
+/Users/hungvx.dev/Dev/Dotfiles/Music/Chen Mo shi Jin.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Chưa Từng Bỏ Cuộc.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Cầm Sư.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Nụ hôn biệt ly.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Song Of Sunset.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Yêu Giang Sơn Càng Yêu Mỹ Nhân.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Thiên Địa Tình Duyên.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Những lời dối gian.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Uyên Ương Hồ Điệp.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Bươm Bướm Bay Đầy Trời.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Tiếu Khán Phong Vân.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/The Memory.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Chờ Trông Ai.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Hoa Liễu Úa Màu.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Sắc Ngọc Ngà.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Thuyền Quyên.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Ví Dầu Đưa Dâu.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/Đừng Trách Con Gái.mp3
