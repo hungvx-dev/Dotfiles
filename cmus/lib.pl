7e8e568d4f5a3b7e5726b81287a2b9bc778e8c68
Vo Xuan Hung <hungvx.dev@gmail.com>
2025-05-03 08:48:24 +0700

    feat(cmus): add musics

--- cmus/lib.pl
+++ cmus/lib.pl
@@ -1,18 +1,25 @@
-/Users/hungvx.dev/Dev/Dotfiles/Music/Chen Mo shi Jin.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Chưa Từng Bỏ Cuộc.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Cầm Sư.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Si Tình Mộ.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Anh Không Hối Hận.mp3
 /Users/hungvx.dev/Dev/Dotfiles/Music/Nụ hôn biệt ly.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Song Of Sunset.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Yêu Giang Sơn Càng Yêu Mỹ Nhân.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Thiên Địa Tình Duyên.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Những lời dối gian.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Uyên Ương Hồ Điệp.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Bươm Bướm Bay Đầy Trời.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Tiếu Khán Phong Vân.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/The Memory.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Chờ Trông Ai.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Hoa Liễu Úa Màu.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Sắc Ngọc Ngà.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Thuyền Quyên.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Ví Dầu Đưa Dâu.mp3
-/Users/hungvx.dev/Dev/Dotfiles/Music/Đừng Trách Con Gái.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Song Of Sunset.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Cầm Sư.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Yêu Giang Sơn Càng Yêu Mỹ Nhân.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Bán Hồ Sa.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/02 Xuân Đình Tuyết.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/03 Nhân Gian Nguyệt Hạ _ 人间月下.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/04 Vạn vật sinh.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/02 Chưa Từng Bỏ Cuộc.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Biệt Thoại.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/02 Im lặng là vàng.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Thiên Địa Tình Duyên.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Uyên Ương Hồ Điệp.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/02 Những lời dối gian.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Bươm Bướm Bay Đầy Trời.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Tiếu Khán Phong Vân.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 The Memory.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Đừng Trách Con Gái.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/02 Hoa Liễu Úa Màu.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/03 Sắc Ngọc Ngà.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Thuyền Quyên.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/02 Ví Dầu Đưa Dâu.mp3
+/Users/hungvx.dev/Dev/Dotfiles/Music/01 Chờ Trông Ai.mp3
