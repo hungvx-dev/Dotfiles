e1dd139c754fadd8f8bfec6202fbbc111beb370e
Vo Xuan Hung <hungvx.dev@gmail.com>
2025-05-21 11:33:07 +0700

    feat(nix): update packages

--- nix-darwin/hungvx.dev/flake.nix
+++ nix-darwin/hungvx.dev/flake.nix
@@ -125,6 +125,7 @@
         };
 
         system = {
+          primaryUser = username;
           configurationRevision = self.rev or self.dirtyRev or null;
           stateVersion = 5;
           defaults = {
