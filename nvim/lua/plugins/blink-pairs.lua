31f19bc3a730e47e66db431de81d4cfaf29e12e0
Vo Xuan Hung <hungvx.dev@gmail.com>
2025-06-14 22:04:38 +0700

    feat(nvim): add blink indent and pairs

--- nvim/lua/plugins/blink-pairs.lua
+++ nvim/lua/plugins/blink-pairs.lua
@@ -1,29 +1,43 @@
 return {
-  "saghen/blink.pairs",
-  enabled = HVIM.plugins.blink_pairs,
-  version = "*", -- (recommended) only required with prebuilt binaries
-  dependencies = "saghen/blink.download",
-  event = { "BufReadPost", "BufNewFile" },
-  -- build = "cargo build --release",
-  --- @module 'blink.pairs'
-  --- @type blink.pairs.Config
-  opts = {
-    mappings = {
-      enabled = true,
-      pairs = {},
-    },
-    highlights = {
-      enabled = true,
-      groups = {
-        "BlinkPairsRed",
-        "BlinkPairsYellow",
-        "BlinkPairsBlue",
-        "BlinkPairsOrange",
-        "BlinkPairsGreen",
-        "BlinkPairsViolet",
-        "BlinkPairsCyan",
+  {
+    "saghen/blink.pairs",
+    enabled = HVIM.plugins.blink_pairs,
+    version = "*", -- (recommended) only required with prebuilt binaries
+    dependencies = "saghen/blink.download",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    --- @module 'blink.pairs'
+    --- @type blink.pairs.Config
+    opts = {
+      mappings = {
+        enabled = true,
+        -- you may also disable with `vim.g.pairs = false` (global) or `vim.b.pairs = false` (per-buffer)
+        disabled_filetypes = {},
+        pairs = {},
       },
+      highlights = {
+        enabled = true,
+        groups = {
+          "BlinkPairsRed",
+          "BlinkPairsYellow",
+          "BlinkPairsBlue",
+          "BlinkPairsOrange",
+          "BlinkPairsGreen",
+          "BlinkPairsViolet",
+          "BlinkPairsCyan",
+        },
+        matchparen = {
+          enabled = true,
+          group = "MatchParen",
+        },
+      },
+      debug = false,
     },
-    debug = false,
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = not HVIM.plugins.blink_pairs,
+    version = false,
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
   },
 }
