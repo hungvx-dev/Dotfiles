ad2f7c65faf5590b10cb6b4fd0706850093f2099
Vo Xuan Hung <hungvx.dev@gmail.com>
2025-11-09 16:58:02 +0700

    feat(nvim): remote nvim/test

--- nvim/test/rust/target/debug/deps/rust-608fdde7e42f01dc.d
+++
@@ -1,9 +0,0 @@
-/Users/hungvx.dev/Dev/Dotfiles/nvim/test/rust/target/debug/deps/librust-608fdde7e42f01dc.rmeta: src/main.rs Cargo.toml
-
-/Users/hungvx.dev/Dev/Dotfiles/nvim/test/rust/target/debug/deps/rust-608fdde7e42f01dc.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- nvim/test/rust/target/debug/deps/rust-9ec96684a4756c9f.d
+++
@@ -1,9 +0,0 @@
-/Users/hungvx.dev/Dev/Dotfiles/nvim/test/rust/target/debug/deps/librust-9ec96684a4756c9f.rmeta: src/main.rs Cargo.toml
-
-/Users/hungvx.dev/Dev/Dotfiles/nvim/test/rust/target/debug/deps/rust-9ec96684a4756c9f.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
