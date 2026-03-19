69c0f05ce30d7e957b7edc43623e16f8e2738633
Vo Xuan Hung <hungvx.dev@gmail.com>
2026-03-19 09:25:13 +0700

    feat(fish): combine fe + be to "dev" alias

--- fish/alias/dev.fish
+++ fish/alias/dev.fish
@@ -1,2 +1,4 @@
-abbr fe 'nix develop ~/Dev/Dotfiles/nix-darwin/hungvx.dev/develop#fe'
-abbr be 'nix develop ~/Dev/Dotfiles/nix-darwin/hungvx.dev/develop#be'
+# abbr fe 'nix develop ~/Dev/Dotfiles/nix-darwin/hungvx.dev/develop#fe'
+# abbr be 'nix develop ~/Dev/Dotfiles/nix-darwin/hungvx.dev/develop#be'
+
+abbr dev 'nix develop ~/Dev/Dotfiles/nix-darwin/hungvx.dev/develop#dev'
--- fish/alias/mix.fish
+++ fish/alias/mix.fish
@@ -6,7 +6,7 @@ abbr nv nvim
 abbr l 'ls -Flha'
 abbr ls 'eza --color=always --icons=always -h -l -g -a --git --time-style=+%Y-%m-%d\ %H:%M'
 abbr ll 'eza --color=always --icons=always -h -l -a --no-filesize --no-user --no-permissions --no-time --git-ignore'
-abbr tree 'eza -T -L=2 --git-ignore'
+abbr tree 'eza -T -L=3 --git-ignore'
 # abbr ll 'ls -Flha'
 # abbr ls 'ls -lag'
 
