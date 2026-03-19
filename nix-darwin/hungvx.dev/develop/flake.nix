f41bf2cfe1ce4e07f4297337dc1305b8bc99ffc1
Vo Xuan Hung <hungvx.dev@gmail.com>
2026-03-19 09:42:47 +0700

    feat(nix): combine fe + be to dev

--- nix-darwin/hungvx.dev/develop/flake.nix
+++ nix-darwin/hungvx.dev/develop/flake.nix
@@ -12,25 +12,10 @@
     in
     {
       devShells.${system} = {
-        fe = pkgs.mkShell {
+        dev = pkgs.mkShell {
           buildInputs = [
-            pkgs.volta
-            pkgs.bun
-          ];
-          shellHook = ''
-            export SHELL=$(which fish)
-               if [ -z "$IN_NIX_SHELL_FISH" ]; then
-                 export IN_NIX_SHELL_FISH=1
-                 exec $SHELL
-               fi
-            echo "🚀 Frontend environment loaded (Node + Bun)"
-          '';
-        };
-
-        be = pkgs.mkShell {
-          buildInputs = [
+            pkgs.mise
             pkgs.rustup
-            pkgs.go
           ];
           shellHook = ''
             export SHELL=$(which fish)
@@ -38,7 +23,6 @@
                  export IN_NIX_SHELL_FISH=1
                  exec $SHELL
                fi
-            echo "🔧 Backend environment loaded (Rust + Go)"
           '';
         };
       };
